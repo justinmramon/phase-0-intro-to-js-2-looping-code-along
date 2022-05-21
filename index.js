9f22bef32b1e1dfff6a23270f45ad5ed5a7ca26f
Justin M. Ramon <justinmramon@gmail.com>
2022-05-20 22:19:15 -0500

    commit tree 7228d623362b105b87ddde9d43c5c57fa922aa81

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, eventName){
+    const array = []
+    for (let c = 0; c < names.length; c++){
+        array.push(`Thank you, ${names[c]}, for the wonderful ${eventName} gift!`);
+    }
+    return array
+}
+function countDown(){
+    let f = 10
+    while (f >= 0){
+        console.log(f)
+        f--;
+    }
+}
